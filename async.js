2fe4b9d721fa453e9b85577c29bcccc9ddd2895b
ChunLiang Zhang <c2u@live.cn>
2012-04-13 14:05:14 +0800

    Draft commit

---
+++ async.js
@@ -0,0 +1,37 @@
+/*
+* Description: Check a object, when it's avaiable exec the function, etc:
+  $async("CompanyNamespace.Package.FunctionName", function(){ 
+      //do something with CompanyNamespace.Package.FunctionName;
+  });
+* Author: Kris Zhang
+* Blog: http://c52u.com
+*/
+
+
+(function(window){
+
+  window.$async = function(tag, func) {
+    var syncId, count = 200,
+        tagAry = tag.split('.'), len = tagAry.length;
+
+    var verify = function(){
+      var i = 0, obj = window;
+
+      do {
+        obj = obj[tagAry[i]]
+        if(!obj) return false;
+      } while(++i < len);
+
+      func();
+      return true;
+    };
+
+    !verify() && (syncId = window.setInterval(function() {
+      if (verify() || !count--) {
+        window.clearInterval(syncId);
+      }
+    }, 150));
+
+  };
+
+})(window);
